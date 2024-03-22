f4b2db0bf91ffbd100e03172af572b0cf4e94251
salparadi <123978356+salparadi@users.noreply.github.com>
2024-03-22 16:05:40 -0400

    Remove test shell scripts for builders.

--- src/cream_bots/builders/liquidity_fetcher.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-python liquidity_fetcher.py arbitrum
-python liquidity_fetcher.py base
-python liquidity_fetcher.py ethereum
-python liquidity_fetcher.py optimism
-python liquidity_fetcher.py polygon
